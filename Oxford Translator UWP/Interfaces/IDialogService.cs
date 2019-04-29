459530d3be2f5b1521a0afe6b9f2c60093437c7d
Papp Bence <pappbence96@gmail.com>
2019-04-29 15:04:45 +0200

    Added Dialog Service to be able to display error messages.

---
+++ Oxford Translator UWP/Interfaces/IDialogService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+
+namespace Oxford_Translator_UWP.Interfaces
+{
+    /// <summary>
+    /// Dialog Service Interface, used to display various modal dialogs
+    /// </summary>
+    public interface IDialogService
+    {
+        /// <summary>
+        /// Display an error dialog with message, return user choice
+        /// </summary>
+        /// <param name="message">error message</param>
+        /// <returns>user choice</returns>
+        IAsyncOperation<ContentDialogResult> ShowError(string message);
+    }
+}
---
+++ Oxford Translator UWP/Services/DialogService.cs
@@ -0,0 +1,28 @@
+﻿using Oxford_Translator_UWP.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+
+namespace Oxford_Translator_UWP.Services
+{
+    /// <summary>
+    /// Concrete implemetation of the IDialogService Interface
+    /// </summary>
+    class DialogService : IDialogService
+    {
+        public IAsyncOperation<ContentDialogResult> ShowError(string message)
+        {
+            var contentDialog = new ContentDialog()
+            {
+                Title = "Error",
+                Content = message,
+                PrimaryButtonText = "Ok"
+            };
+            return contentDialog.ShowAsync();
+        }
+    }
+}
